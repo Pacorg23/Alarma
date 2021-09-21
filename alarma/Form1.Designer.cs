af44dda3c9eaa49145c3ab77ca8e3ad008f88a6b
pacorg <frg31415@gmail.com>
2021-09-21 11:40:03 -0500

    Agregar archivos de proyecto.

---
+++ alarma/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace alarma
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.start = new System.Windows.Forms.Button();
+            this.TiempoTotal = new System.Windows.Forms.Label();
+            this.deficitDeTiempo = new System.Windows.Forms.Label();
+            this.stop = new System.Windows.Forms.Button();
+            this.lap = new System.Windows.Forms.Button();
+            this.lapList = new System.Windows.Forms.ListBox();
+            this.currentLap = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(28, 26);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 0;
+            this.start.Text = "Iniciar";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TiempoTotal
+            // 
+            this.TiempoTotal.AutoSize = true;
+            this.TiempoTotal.Location = new System.Drawing.Point(157, 26);
+            this.TiempoTotal.Name = "TiempoTotal";
+            this.TiempoTotal.Size = new System.Drawing.Size(49, 15);
+            this.TiempoTotal.TabIndex = 1;
+            this.TiempoTotal.Text = "00:00:00";
+            this.TiempoTotal.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // deficitDeTiempo
+            // 
+            this.deficitDeTiempo.AutoSize = true;
+            this.deficitDeTiempo.Location = new System.Drawing.Point(157, 148);
+            this.deficitDeTiempo.Name = "deficitDeTiempo";
+            this.deficitDeTiempo.Size = new System.Drawing.Size(49, 15);
+            this.deficitDeTiempo.TabIndex = 2;
+            this.deficitDeTiempo.Text = "00:00:00";
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(28, 85);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(75, 23);
+            this.stop.TabIndex = 3;
+            this.stop.Text = "Parar";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // lap
+            // 
+            this.lap.Location = new System.Drawing.Point(28, 140);
+            this.lap.Name = "lap";
+            this.lap.Size = new System.Drawing.Size(75, 23);
+            this.lap.TabIndex = 4;
+            this.lap.Text = "Vuelta";
+            this.lap.UseVisualStyleBackColor = true;
+            this.lap.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lapList
+            // 
+            this.lapList.AllowDrop = true;
+            this.lapList.FormattingEnabled = true;
+            this.lapList.ItemHeight = 15;
+            this.lapList.Location = new System.Drawing.Point(262, 6);
+            this.lapList.Name = "lapList";
+            this.lapList.Size = new System.Drawing.Size(161, 199);
+            this.lapList.TabIndex = 5;
+            // 
+            // currentLap
+            // 
+            this.currentLap.AutoSize = true;
+            this.currentLap.Location = new System.Drawing.Point(157, 85);
+            this.currentLap.Name = "currentLap";
+            this.currentLap.Size = new System.Drawing.Size(49, 15);
+            this.currentLap.TabIndex = 6;
+            this.currentLap.Text = "00:00:00";
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 211);
+            this.Controls.Add(this.currentLap);
+            this.Controls.Add(this.lapList);
+            this.Controls.Add(this.lap);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.deficitDeTiempo);
+            this.Controls.Add(this.TiempoTotal);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label TiempoTotal;
+        private System.Windows.Forms.Label deficitDeTiempo;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button lap;
+        private System.Windows.Forms.ListBox lapList;
+        private System.Windows.Forms.Label currentLap;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ alarma/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace alarma
+{
+    public partial class Form1 : Form
+    {
+        
+        public int tiempoTotal=0;
+        public int vueltaActual=0;
+        public int adeudoDeTiempo { get; set; }
+        System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\okey-\source\repos\alarma\alarma\sonidos\mixkit-digital-clock-digital-alarm-buzzer-992.wav");
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            timer.Interval = 1000;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!timer.Enabled)
+            {
+                timer.Start();
+
+            }
+            
+            
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            tiempoTotal++;
+            vueltaActual++;
+            if (vueltaActual>=2700)
+            {
+                if (vueltaActual%60==0)
+                {
+                    player.Play();
+                }
+                if (vueltaActual>27600)
+                {
+                    currentLap.BackColor = Color.IndianRed;
+
+                }
+            }
+            TiempoTotal.Text = deIntATiempo(tiempoTotal);
+            currentLap.Text = deIntATiempo(vueltaActual);
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            if (vueltaActual>27600)
+            {
+                adeudoDeTiempo = adeudoDeTiempo + (vueltaActual - 27000);
+                deficitDeTiempo.Text = deIntATiempo(adeudoDeTiempo);
+            }
+            lapList.Items.Add(deIntATiempo(vueltaActual));
+            vueltaActual = 0;
+            currentLap.Text = deIntATiempo(0);
+            timer.Start();
+        }
+        private string deIntATiempo(int tiempo)
+        {
+            
+            
+            int segundos, minutos, horas;
+            segundos = tiempo % 60;
+            tiempo = tiempo - segundos;
+            minutos = tiempo / 60;
+            horas = minutos / 60;
+            minutos = minutos - (horas * 60);
+            TimeSpan time = new TimeSpan(0, horas, minutos, segundos);
+            string output=time.ToString(@"dd\.hh\:mm\:ss");
+            
+            return output.Remove(0, 3);
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            if (!timer.Enabled)
+            {
+                
+                
+                if (vueltaActual==0)
+                {
+                    tiempoTotal = 0;
+                }
+                vueltaActual = 0;
+            }
+
+        }
+    }
+}
